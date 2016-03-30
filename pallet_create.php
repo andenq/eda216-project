2a219a22d262dedc10d5a38d514bf973153a361f
andreas.coroiu@gmail.com <andreas.coroiu@gmail.com>
2016-03-30 11:33:04 +0200

    Continued work on pallet_create.

--- pallet_create.php
+++ pallet_create.php
@@ -3,12 +3,20 @@
 
 <div class="container">
     <h2 class="page-header">Create (scan) pallet</h2>
-    <form>
-        <div class="form-group">
-            <label for="barcode_id">Barcode ID</label>
-            <input type="text" class="form-control" id="barcode_id" name="barcode_id" placeholder="Barcode ID">
+    <div class="row">
+        <div class="col-xs-6">
+            <form>
+                <div class="form-group">
+                    <label for="barcode_id">Barcode ID</label>
+                    <input type="text" class="form-control" id="barcode_id" name="barcode_id" placeholder="Barcode ID">
+                </div>
+                <div class="form-group">
+                    <label for="pastry_name">Pastry name</label>
+                    <?php select_tag(array_map(function($p) {return $p['name'];}, $db->getPastries()), false); ?>
+                </div>
+            </form>
         </div>
-    </form>
+    </div>
 </div>
 
 <?php include('footer.php'); ?> 
